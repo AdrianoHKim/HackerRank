5b8a937402fc0546d504afef3db4084ce1bf2937
kimzera <adrianohkim@gmail.com>
2022-12-26 15:21:02 -0300

    Prepare > Python Exercise: itertools.permutations() Difficulty: Easy Status: Done

--- tests.py
+++
@@ -1,8 +0,0 @@
-from itertools import product
-A = input().split()
-A = list(map(int, A))
-B = input().split()
-B = list(map(int, B))
-output = list(product(A, B))
-for i in output:
-    print(i, end=" ")
