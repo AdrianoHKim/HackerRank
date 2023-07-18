d9f7b9518df032d27a2237c0bec1c2e1b0407826
kimzera <adrianohkim@gmail.com>
2023-07-18 10:35:14 -0300

    Prepare > Python Exercise: alphabet rangoli Difficulty: Easy Status: Done Obs.: Task added to the code

--- easy/ez_alphabet_rangoli.py
+++ easy/ez_alphabet_rangoli.py
@@ -1,3 +1,33 @@
+"""
+Task:
+
+You are given an integer, n .
+Your task is to print an alphabet rangoli of size n .
+(Rangoli is a form of Indian folk art based on creation of patterns.)
+
+Different sizes of alphabet rangoli are shown below:
+
+#size 3
+
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+
+#size 5
+
+--------e--------
+------e-d-e------
+----e-d-c-d-e----
+--e-d-c-b-c-d-e--
+e-d-c-b-a-b-c-d-e
+--e-d-c-b-c-d-e--
+----e-d-c-d-e----
+------e-d-e------
+--------e--------
+
+"""
 def print_rangoli(size):
     alpha = "abcdefghijklmnopqrstuvwxyz"
     data = [alpha[i] for i in range(size)]
